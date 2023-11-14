2d8780a2b7e214e3794a3fedfa77ab4f046d0408
WernerLDev <7613278+WernerLDev@users.noreply.github.com>
2023-11-14 19:03:03 +0100

    Improve footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,23 +8,35 @@ export const Footer = () => {
         <small>
           <span>
             Copyright &copy; {format(new Date(), "yyyy")} - Werner Langenhuisen
-            |{" "}
             <a
               href="https://github.com/WernerLDev/werlang.nl"
               target="_blank"
               rel="noreferrer"
+              className="ps-3 d-none d-lg-inline"
             >
               View source
             </a>
           </span>
         </small>
-        <div className="fs-5">
-          <a rel="author" href={PERSONAL_INFO.GithubUrl}>
-            <i className="bi bi-github"></i>
-          </a>{" "}
-          <a rel="author" href={PERSONAL_INFO.LinkedinUrl}>
-            <i className="bi bi-linkedin"></i>
-          </a>
+        <div className="fs-5 d-flex d-lg-block justify-content-between">
+          <small>
+            <a
+              href="https://github.com/WernerLDev/werlang.nl"
+              target="_blank"
+              rel="noreferrer"
+              className="pe-3 d-inline d-lg-none"
+            >
+              View source
+            </a>
+          </small>
+          <span>
+            <a rel="author" href={PERSONAL_INFO.GithubUrl}>
+              <i className="bi bi-github"></i>
+            </a>{" "}
+            <a rel="author" href={PERSONAL_INFO.LinkedinUrl}>
+              <i className="bi bi-linkedin"></i>
+            </a>
+          </span>
         </div>
       </div>
     </footer>
