74c656b6afff51c8bd66bf0b058ffe878d2968fa
WernerLDev <7613278+WernerLDev@users.noreply.github.com>
2024-09-18 19:27:59 +0200

    Update packages and add view transition

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -5,17 +5,26 @@ import { Form } from "react-bootstrap";
 export const ThemeSwitcher = () => {
   const [theme, setTheme] = useContext(ThemeContext);
 
+  const switchTheme = (theme: WebsiteTheme) => () => {
+    setTheme(theme);
+  };
+
   return (
     <Form.Check // prettier-ignore
       type="switch"
       id="theme-switcher"
       label="Dark mode"
+      aria-pressed={theme == "dark"}
       checked={theme === "dark"}
       onChange={(e) => {
         const newTheme: WebsiteTheme = e.currentTarget.checked
           ? "dark"
           : "light";
-        setTheme(newTheme);
+        if (!document.startViewTransition) {
+          switchTheme(newTheme)();
+        } else {
+          document.startViewTransition(switchTheme(newTheme));
+        }
       }}
     />
   );
